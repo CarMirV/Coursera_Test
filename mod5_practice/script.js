55428973d75b1e35f89614d91f2b213a34bae3af
Carlos Miranda <carlosmiranda@MacBook-Pro.local>
2020-05-12 09:47:41 -0500

    Coursera

---
+++ mod5_practice/script.js
@@ -0,0 +1,34 @@
+//DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    function sayHello (event) {
+        this.textContent = "Said it!";
+        var name = document.getElementById("name").value;
+        var message = "<h2>Hello " + name + "!</h2>";
+    
+        // document.getElementById("content").textContent = message;
+        document.getElementById("content").innerHTML = message;
+    
+        if(name === "student") {
+            var title = document.querySelector("#title").textContent;
+            title += " & lovin' it!";
+    
+            document.querySelector("#title").textContent = title;
+        }
+    }
+    document.querySelector("button").addEventListener("click", sayHello);
+
+    document.querySelector("body").addEventListener("mousemove", function (event) {
+        if(event.shiftKey == true) {
+        console.log(event.clientX);
+        console.log(event.clientY);
+        }
+    });
+
+});
+
+
+
